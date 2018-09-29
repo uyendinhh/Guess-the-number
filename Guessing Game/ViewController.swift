3b4b07e11a6dd43f9e67a1080c28dcbebd99069a
Uyen Dinh <udinh3@gatech.edu>
2018-09-29 14:21:49 -0400

    Initial Commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Uyen Dinh on 9/29/18.
+//  Copyright © 2018 Uyen Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Guessing GameTests/Guessing_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guessing_GameTests.swift
+//  Guessing GameTests
+//
+//  Created by Uyen Dinh on 9/29/18.
+//  Copyright © 2018 Uyen Dinh. All rights reserved.
+//
+
+import XCTest
+@testable import Guessing_Game
+
+class Guessing_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
